a493665927c2f3493bb9c4e106da7dd461d4f380
Yuri <yuricoelho33@gmail.com>
2021-05-09 19:11:53 -0300

    Adicionando expressões regulares.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
 from ExtratorArgumentosUrl import ExtratorArgumentosUrl
+import re
 url = "https://www.bytebank.com.br/cambio?moedaorigem=real&moedadestino=dolar&valor=150"
 cambio = ExtratorArgumentosUrl(url)
 moedaOrigem, moedaDestino = cambio.retornaMoedas()
@@ -10,5 +10,10 @@ cambio = ExtratorArgumentosUrl(url)
 moedaOrigem, moedaDestino = cambio.retornaMoedas()
 valor = cambio.retornaValor()
 print(moedaOrigem, moedaDestino, valor)
+
+padrao = "[0-9]{4,5}-?[0-9]{4}"
+texto = "Meu número para contato é 2633-5723"
+retorno = re.search(padrao,texto)
+print(retorno.group())
 
 
