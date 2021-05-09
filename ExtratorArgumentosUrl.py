d713a8826619b21a9ec7ffb17a7e83b45b19d8ab
Yuri <yuricoelho33@gmail.com>
2021-05-09 18:44:33 -0300

    Adicionando a validação da URL.

--- ExtratorArgumentosUrl.py
+++ ExtratorArgumentosUrl.py
@@ -1,7 +1,7 @@
-class ExtratorArgumentosUrl
+class ExtratorArgumentosUrl:
     def __init__(self, url):
         if self.stringEhValida(url):
-            self.url = url
+            self.url = url.lower()
         else:
             raise LookupError("Url inválida")
     @staticmethod
@@ -17,6 +17,8 @@ class ExtratorArgumentosUrl
         inicioSubstringMoedaOrigem = self.encontraIndiceInicioSubstring(buscaMoedaOrigem)
         finalSubstringMoedaOrigem = self.url.find("&")
         moedaOrigem = self.url[inicioSubstringMoedaOrigem : finalSubstringMoedaOrigem]
+        if moedaOrigem == "moedadestino":
+            moedaOrigem = self.verificaMoedaOrigem(buscaMoedaOrigem)
 
         inicioSubstringMoedaDestino = self.encontraIndiceInicioSubstring(buscaMoedaDestino)
         finalSubstringMoedaDestino = self.url.find("&valor")
@@ -26,3 +28,15 @@ class ExtratorArgumentosUrl
 
     def encontraIndiceInicioSubstring(self, moedaOuValor):
         return self.url.find(moedaOuValor) + len(moedaOuValor) + 1
+
+    def verificaMoedaOrigem(self, buscaMoedaOrigem):
+        self.url = self.url.replace("moedadestino", "real", 1)
+        inicioSubstringMoedaOrigem = self.encontraIndiceInicioSubstring(buscaMoedaOrigem)
+        finalSubstringMoedaOrigem = self.url.find("&")
+        return self.url[inicioSubstringMoedaOrigem:finalSubstringMoedaOrigem]
+
+    def retornaValor(self):
+        buscaValor = "Valor".lower()
+        inicioSubstringValor = self.encontraIndiceInicioSubstring(buscaValor)
+        valor = self.url[inicioSubstringValor:]
+        return valor
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 
-# argumento = "www.bytebank.com/cambio?moedaorigem=real"
-# substring = argumento[5:11]
-# print(substring)
+from ExtratorArgumentosUrl import ExtratorArgumentosUrl
+url = "https://www.bytebank.com.br/cambio?moedaorigem=real&moedadestino=dolar&valor=150"
+cambio = ExtratorArgumentosUrl(url)
+moedaOrigem, moedaDestino = cambio.retornaMoedas()
+valor = cambio.retornaValor()
+print(moedaOrigem, moedaDestino, valor)
+url = "https://www.bytebank.com.br/cambio?moedaorigem=moedadestino&moedadestino=dolar&valor=150"
+cambio = ExtratorArgumentosUrl(url)
+moedaOrigem, moedaDestino = cambio.retornaMoedas()
+valor = cambio.retornaValor()
+print(moedaOrigem, moedaDestino, valor)
 
-argumento = "moedaorigem=real"
-lista_argumentos = argumento.split("=")
-print(lista_argumentos)
+
