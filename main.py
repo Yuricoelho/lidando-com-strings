36efabefd5b431d608a0d8097709b78b784044fe
Yuri <yuricoelho33@gmail.com>
2021-05-09 14:01:32 -0300

    Demostrando o fatiamento de strings em Python com uma url.

---
+++ main.py
@@ -0,0 +1,8 @@
+
+# argumento = "www.bytebank.com/cambio?moedaorigem=real"
+# substring = argumento[5:11]
+# print(substring)
+
+argumento = "moedaorigem=real"
+lista_argumentos = argumento.split("=")
+print(lista_argumentos)
